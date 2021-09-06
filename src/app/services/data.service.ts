957c8581c13fddd3cc6fa8094eae14f477f115f4
Your Name <venkatkiran343@gmail.com>
2021-09-06 21:24:17 +0530

    first chenges

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,6 +6,14 @@ export interface Message {
   date: string;
   id: number;
   read: boolean;
+  imageUrl: string;
+  price: string;
+  place: string;
+  qualification1: string;
+  qualification2: string;
+  qualification3: string;
+  qualification4: string;
+  aboutJob: string;
 }
 
 @Injectable({
@@ -14,61 +22,66 @@ export interface Message {
 export class DataService {
   public messages: Message[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
+      fromName: 'Google',
+      subject: 'Full time UI Designer',
+      date: '5h',
       id: 0,
+      imageUrl: 'https://i.pinimg.com/originals/39/21/6d/39216d73519bca962bd4a01f3e8f4a4b.png',
+      price: '$20k - ',
+      place: ' Tokya,Japan',
+      qualification1: '- Bechelor s degree or equivalent practical experince.',
+      qualification2: '- 8 years of techincal UI designer experience such as creating strategic product.',
+      qualification3: '- Experience driving product vision, go-to-market strategy, and design discussions.',
+      qualification4: '- Experience developing internet products and technologies.',
+      aboutJob: 'Work closely with Business Analysts to translate approach notes into wireframes and mockups that lead to intuitive.',
       read: false
     },
     {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
+      fromName: 'Amazon',
+      subject: 'Product Designer',
+      date: '22h',
       id: 1,
+      imageUrl: 'https://sm.mashable.com/mashable_in/image/default/uploads252fcard252fimage252f1603703252f270775c5-1af8-4bce-96_t2pb.png',
+      price: '$10k - ',
+      place: ' Tokya,Japan',
+      qualification1: '- Bechelor s degree or equivalent practical experince.',
+      qualification2: '- 8 years of techincal UI designer experience such as creating strategic product.',
+      qualification3: '- Experience driving product vision, go-to-market strategy, and design discussions.',
+      qualification4: '- Experience developing internet products and technologies.',
+      aboutJob: 'Work closely with Business Analysts to translate approach notes into wireframes and mockups that lead to intuitive.',
       read: false
     },
     {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
+      fromName: 'Facebook',
+      subject: 'UX Designer',
+      date: '4d',
       id: 2,
+      imageUrl: 'https://i.pinimg.com/originals/d5/18/ec/d518eceea19f4b1a2ee032ddc634dd7d.jpg',
+      price: '$14k - ',
+      place: ' Tokya,Japan',
+      qualification1: '- Bechelor s degree or equivalent practical experince.',
+      qualification2: '- 8 years of techincal UI designer experience such as creating strategic product.',
+      qualification3: '- Experience driving product vision, go-to-market strategy, and design discussions.',
+      qualification4: '- Experience developing internet products and technologies.',
+      aboutJob: 'Work closely with Business Analysts to translate approach notes into wireframes and mockups that lead to intuitive.',
       read: false
     },
     {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
+      fromName: 'Swiggy',
+      subject: 'Full time UI Designer',
+      date: '9d',
       id: 3,
+      imageUrl: 'https://www.deccanherald.com/sites/dh/files/articleimages/2020/07/06/Swiggy-1594035877.jpg',
+      price: '$40k - ',
+      place: ' Tokya,Japan',
+      qualification1: '- Bechelor s degree or equivalent practical experince.',
+      qualification2: '- 8 years of techincal UI designer experience such as creating strategic product.',
+      qualification3: '- Experience driving product vision, go-to-market strategy, and design discussions.',
+      qualification4: '- Experience developing internet products and technologies.',
+      aboutJob: 'Work closely with Business Analysts to translate approach notes into wireframes and mockups that lead to intuitive.',
       read: false
     },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
-    }
+
   ];
 
   constructor() { }
